0a16444673576314cc0502e2768b17d7603adb11
mym0404 <mym0404@gmail.com>
2024-03-13 11:03:46 +0900

    [bot] Auto Commit

---
+++ doc/docusaurus.config.ts
@@ -0,0 +1,128 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const title = 'React Native Styled System';
+const description = 'styled-system mimic for React Native';
+const websiteUrl = 'https://mj-studio-library.github.io/react-native-styled-system/';
+const repoOrg = 'mj-studio-library';
+const repoName = 'react-native-styled-system';
+const repoUrl = 'https://github.com/mj-studio-library/react-native-styled-system';
+const coverImage = 'img/ogimage.png';
+
+const config: Config = {
+  title: title,
+  tagline: description,
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://mj-studio-library.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/react-native-styled-system/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: repoOrg, // Usually your GitHub org/user name.
+  projectName: repoName, // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: coverImage,
+    navbar: {
+      title: title,
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          type: 'localeDropdown',
+        },
+        {
+          href: repoUrl,
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: repoUrl,
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MJ Studio.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+
+  plugins: ['docusaurus-plugin-sass'],
+};
+
+export default config;
---
+++ doc/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className={'container'} />
+    </section>
+  );
+}
