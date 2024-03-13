43daed5ec10ee65197085103dccff4c3f250f74b
mym0404 <mym0404@gmail.com>
2024-03-13 10:26:53 +0900

    [bot] Auto Commit

--- example/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@mj-studio/eslint-config-react'],
-};
--- example/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'all',
-  arrowParens: 'always',
-  singleQuote: true,
-  jsxSingleQuote: false,
-  quoteProps: 'consistent',
-  printWidth: 100,
-};
