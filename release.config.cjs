c4b73326a6bdc58dae9e5b47544f78281e7ac629
mym0404 <mym0404@gmail.com>
2024-03-14 13:59:15 +0900

    ci: github workflow

---
+++ release.config.cjs
@@ -0,0 +1,7 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  repositoryUrl: 'https://github.com/mj-studio-library/react-native-styled-system'
+};
