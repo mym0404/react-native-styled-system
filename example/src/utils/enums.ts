1ad2e43169b88cd64cfa0d8b5601e9e17f27fe9d
mym0404 <mym0404@gmail.com>
2024-03-21 23:49:32 +0900

    build: remove unused codes in example

--- example/src/utils/enums.ts
+++
@@ -1,9 +0,0 @@
-export enum SCREENS {
-  EXAMPLES_DIRECTORY = 'ExamplesDirectory',
-  HEADER_NAVBAR_EXAMPLE = 'HeaderNavBarExample',
-  SAFE_AREA_VIEW_EXAMPLE = 'SafeAreaViewExample',
-  IMAGE_FOREGROUND_EXAMPLE = 'ImageForegroundExample',
-  ONLY_FOREGROUND_EXAMPLE = 'OnlyForegroundExample',
-  REFRESH_CONTROL_EXAMPLE = 'RefreshControlExample',
-  HEADER_NAVBAR_FLATLIST_EXAMPLE = 'HeaderNavbarFlatListExample',
-}
--- example/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import { Platform } from 'react-native';
-
-export const isIOS = Platform.OS === 'ios';
-
-export const data = Array.from(Array(20).keys());
