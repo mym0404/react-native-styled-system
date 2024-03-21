0816b7c6d0b900a7e4283bf74efe50ef5ecd48bd
mym0404 <mym0404@gmail.com>
2024-03-22 00:34:17 +0900

    build: wip

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { Home } from './screen/Home';
+import { AppThemeProvider } from './theme/AppThemeProvider';
+
 const App = () => {
-  return <SafeAreaProvider />;
+  return (
+    <SafeAreaProvider>
+      <AppThemeProvider>
+        <Home />
+      </AppThemeProvider>
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
---
+++ example/src/screen/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+type HomeProps = {};
+
+const Home = ({}: HomeProps) => {
+  return <Text>{'Home'}</Text>;
+};
+
+export { Home };
+export type { HomeProps };
